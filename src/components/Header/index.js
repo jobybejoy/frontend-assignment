d499bc11cd606235cbb9396e3639b846d6511b9d
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-06-12 11:32:44 -0400

    Added Search Input

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
 import logo from '../../images/logo.svg';
 import "./Header.css"
 
-function Header() {
+function Header({ query, onChangeHander }) {
   return (
     <header className="header">
       <img src={logo} alt="Timescale" />
-      {/* <input type="text" /> */}
+      <input className="search_input" type="text" name="search" placeholder="Search for a movie" value={query} onChange={onChangeHander} />
     </header>
   )
 }
