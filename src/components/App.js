55bd050da2fee61a9e3bfd276da780c798a71d78
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-06-11 21:05:58 -0400

    App base

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from "react"
+import Header from "./Header"
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+import "./App.css"
+
+function App() {
+
+	return (
+		<div className="page_container">
+			<Header />
+			<main>
+				<h3 className="section_title">Most Recent Movies</h3>
+
+			</main>
+		</div>
+	)
+
+}
 
 export default App;
