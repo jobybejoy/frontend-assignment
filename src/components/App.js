eeea33c4d688204e54bfc7f73d65563d436b50d7
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-06-14 13:13:29 -0400

    Added MediaQueries

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Header from "./Header"
 import Card from "./Card"
+import Modal from "./Modal"
 
 import "./App.css"
 
@@ -8,24 +9,22 @@ function App() {
 	const [movies, setMovies] = useState([])
 	const [query, setSearchQuery] = useState("")
 
+	const [selectedMovie, setSelectedMovie] = useState({})
+	const [isModalOpen, setModalOpen] = useState(false)
+
 	useEffect(() => {
 		fetchMovies();
-		return () => {
-			setMovies([])
-		}
+		return () => { setMovies([]) }
 	}, [query === ""]);
 
-	useEffect(() => {
-		searchMovies(query)
-	}, [query])
+	useEffect(() => { searchMovies(query) }, [query])
 
 	async function fetchMovies() {
-
 		const config = {
 			method: "GET",
 			headers: { "Content-Type": "application/json" }
 		};
-		return await fetch(`${process.env.REACT_APP_API_DOMAIN}/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&query=${query}`, config)
+		return await fetch(`${process.env.REACT_APP_API_DOMAIN}/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`, config)
 			.then(response => (response.json()))
 			.then(response => (response.results))
 			.then(movies => setMovies(movies))
@@ -43,13 +42,20 @@ function App() {
 			.then(movies => setMovies(movies))
 	}
 
-
-
 	const searchInpuHandler = (e) => {
 		setSearchQuery(e.target.value);
 	}
 
+	const onCardClickHandler = (id) => {
+		const current_movie = movies.find((movie) => movie.id === id)
+		setSelectedMovie(current_movie)
+		setModalOpen(true)
+	}
 
+	const onModalClose = () => {
+		setModalOpen(false)
+		setSelectedMovie({})
+	}
 
 	if (!movies) {
 		return "Loading"
@@ -59,7 +65,7 @@ function App() {
 		<div className="page_container">
 			<Header query={query} onChangeHander={searchInpuHandler} />
 			<main>
-				<h3 className="section_title">Most Recent Movies</h3>
+				<h3 className="section_title">{query ? `Searching for ${query}` : "Most Recent Movies"}</h3>
 				<section className="movies_container">
 					{
 						movies.map((movie) => {
@@ -68,12 +74,15 @@ function App() {
 									key={movie.id}
 									rating={movie.vote_average}
 									img_src={process.env.REACT_APP_API_BASE_IMAGE_URL + movie.poster_path}
-									img_alt="" />
+									img_alt={movie.overview}
+									onClick={() => onCardClickHandler(movie.id)}
+								/>
 							)
 						})
 					}
 				</section>
 			</main>
+			<Modal movie={selectedMovie} isModalOpen={isModalOpen} onModalClose={onModalClose} />
 		</div>
 	)
 
